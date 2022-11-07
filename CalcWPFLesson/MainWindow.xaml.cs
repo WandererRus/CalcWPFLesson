b42ca100ad703edb392997f3dbd9caba3b50f181
Fastov Ilya Olegovich <iluxailuxa@gmail.com>
2022-11-07 20:55:22 +0300

    321

--- CalcWPFLesson/MainWindow.xaml.cs
+++ CalcWPFLesson/MainWindow.xaml.cs
@@ -24,5 +24,55 @@ namespace CalcWPFLesson
         {
             InitializeComponent();
         }
+
+        private void btn_zero_Click(object sender, RoutedEventArgs e)
+        {
+            tb_currentResult.Text += "0";
+        }
+
+        private void btn_one_Click(object sender, RoutedEventArgs e)
+        {
+            tb_currentResult.Text += "1";
+        }
+
+        private void btn_two_Click(object sender, RoutedEventArgs e)
+        {
+            tb_currentResult.Text += "2";
+        }
+
+        private void btn_three_Click(object sender, RoutedEventArgs e)
+        {
+            tb_currentResult.Text += "3";
+        }
+
+        private void btn_four_Click(object sender, RoutedEventArgs e)
+        {
+            tb_currentResult.Text += "4";
+        }
+
+        private void btn_five_Click(object sender, RoutedEventArgs e)
+        {
+            tb_currentResult.Text += "5";
+        }
+
+        private void btn_six_Click(object sender, RoutedEventArgs e)
+        {
+            tb_currentResult.Text += "6";
+        }
+
+        private void btn_seven_Click(object sender, RoutedEventArgs e)
+        {
+            tb_currentResult.Text += "7";
+        }
+
+        private void btn_eight_Click(object sender, RoutedEventArgs e)
+        {
+            tb_currentResult.Text += "8";
+        }
+
+        private void btn_nine_Click(object sender, RoutedEventArgs e)
+        {
+            tb_currentResult.Text += "9";
+        }
     }
 }
