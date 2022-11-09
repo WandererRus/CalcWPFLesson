77fc67940eee97526a0743e5961400abb3b97243
Fastov Ilya Olegovich <iluxailuxa@gmail.com>
2022-11-09 19:44:21 +0300

    123

--- CalcWPFLesson/MainWindow.xaml.cs
+++ CalcWPFLesson/MainWindow.xaml.cs
@@ -25,6 +25,7 @@ namespace CalcWPFLesson
             InitializeComponent();
         }
         double firstNumber = 0.0, secondNumber = 0.0;
+        string oper = "";
         private void btn_zero_Click(object sender, RoutedEventArgs e)
         {
             tb_currentResult.Text += "0";
@@ -77,13 +78,13 @@ namespace CalcWPFLesson
 
         private void btn_point_Click(object sender, RoutedEventArgs e)
         {
-            if (tb_currentResult.Text.Length != 0 && !tb_currentResult.Text.Contains("."))
+            if (tb_currentResult.Text.Length != 0 && !tb_currentResult.Text.Contains(","))
             {
-                tb_currentResult.Text += ".";
+                tb_currentResult.Text += ",";
             }
             else if (tb_currentResult.Text.Length == 0)
             {
-                tb_currentResult.Text += "0.";
+                tb_currentResult.Text += "0,";
             }
         }
 
@@ -99,6 +100,7 @@ namespace CalcWPFLesson
             tb_globalResult.Text = "";
             secondNumber = 0.0;
             firstNumber = 0.0;
+
         }
 
         private void btn_lastCharDelete_Click(object sender, RoutedEventArgs e)
@@ -109,71 +111,76 @@ namespace CalcWPFLesson
 
         private void btn_divide_Click(object sender, RoutedEventArgs e)
         {
-            if (tb_currentResult.Text.Length != 0 && tb_currentResult.Text[tb_currentResult.Text.Length - 1] != '.')
-            {
-                tb_currentResult.Text += " / ";
-                tb_globalResult.Text += tb_currentResult.Text;
-                tb_currentResult.Text = "";
-            }
+            if (tb_currentResult.Text.Length > 0 && btn_divide.Content.ToString() != oper)
+                OperationResult(ref firstNumber, ref secondNumber, oper, ref tb_currentResult, ref tb_globalResult, false);
+            oper = "/";
+            OperationResult(ref firstNumber, ref secondNumber, oper, ref tb_currentResult, ref tb_globalResult, false);
         }
 
         private void btn_multiple_Click(object sender, RoutedEventArgs e)
         {
-            if (tb_currentResult.Text.Length != 0 && tb_currentResult.Text[tb_currentResult.Text.Length - 1] != '.')
-            {
-                tb_currentResult.Text += " * ";
-                tb_globalResult.Text += tb_currentResult.Text;
-                tb_currentResult.Text = "";
-            }
+            if (tb_currentResult.Text.Length > 0 && btn_multiple.Content.ToString() != oper)
+                OperationResult(ref firstNumber, ref secondNumber, oper, ref tb_currentResult, ref tb_globalResult, false);
+            oper = "*";
+            OperationResult(ref firstNumber, ref secondNumber, oper, ref tb_currentResult, ref tb_globalResult, false);
         }
 
         private void btn_sub_Click(object sender, RoutedEventArgs e)
         {
-            if (tb_currentResult.Text.Length != 0 && tb_currentResult.Text[tb_currentResult.Text.Length - 1] != '.')
-            {
-                tb_currentResult.Text += " - ";
-                tb_globalResult.Text += tb_currentResult.Text;
-                tb_currentResult.Text = "";
-            }
+            if (tb_currentResult.Text.Length > 0 && btn_sub.Content.ToString() != oper)
+                OperationResult(ref firstNumber, ref secondNumber, oper, ref tb_currentResult, ref tb_globalResult, false);
+            oper = "-";
+            OperationResult(ref firstNumber, ref secondNumber, oper, ref tb_currentResult, ref tb_globalResult, false);
         }
 
         private void btn_add_Click(object sender, RoutedEventArgs e)
         {
-            if (tb_currentResult.Text.Length != 0 && tb_currentResult.Text[tb_currentResult.Text.Length - 1] != '.')
-            {
-                if (firstNumber == 0.0)
-                {
-                    firstNumber = Double.Parse(tb_currentResult.Text);
-                }
-                else
-                {
-                    secondNumber = Double.Parse(tb_currentResult.Text);
-                }
-                if (firstNumber > 0 && secondNumber > 0)
-                {
-                    firstNumber += secondNumber;
-                    tb_globalResult.Text = "";
-                    tb_globalResult.Text += firstNumber + "+";
-                    secondNumber = 0.0;
-                }
-                else 
-                {
-                    tb_globalResult.Text += firstNumber + "+";
-                }
-                tb_currentResult.Text = "";
-            }
+            if (tb_currentResult.Text.Length > 0 && btn_add.Content.ToString() != oper)
+                OperationResult(ref firstNumber, ref secondNumber, oper, ref tb_currentResult, ref tb_globalResult, false);
+            oper = "+";
+            OperationResult(ref firstNumber, ref secondNumber, oper, ref tb_currentResult, ref tb_globalResult, false);
         }
 
         private void btn_equal_Click(object sender, RoutedEventArgs e)
         {
-            //попробовать распарсить сложные выражения
-            
+            OperationResult(ref firstNumber, ref secondNumber, oper, ref tb_currentResult, ref tb_globalResult, true);
         }
 
 
-        static double OperationResult(int n1, int n2, string operation)
+        static void OperationResult(ref double n1, ref double n2, string operation, ref TextBox current, ref TextBox global, bool end)
         {
-            return 0.0;
+            if (current.Text.Length != 0 && current.Text[current.Text.Length - 1] != ',')
+            {
+                if (n1 == 0.0)
+                {
+                    n1 = Double.Parse(current.Text);
+                }
+                else
+                {
+                    n2 = Double.Parse(current.Text);
+                }
+                if (n1 != 0.0 && n2 != 0.0)
+                {
+                    switch (operation)
+                    {
+                        case "+": n1 += n2; break;
+                        case "-": n1 -= n2; break;
+                        case "*": n1 *= n2; break;
+                        case "/": n1 /= n2; break;
+                    }
+                    if (end) global.Text = n1.ToString(); if (!end) global.Text = n1 + operation;
+                    n2 = 0.0;
+                }
+                else
+                {
+                    global.Text = n1 + operation;
+                }
+                current.Text = "";
+            }
+            else if (!global.Text.Contains(operation))
+            {
+                global.Text = n1 + operation;
+            }
         }
     }
 }
